92e4841fb5ca9cebd7e8eced7264237b4e8e4d51
Param Thakkar <pht0104@gmail.com>
2019-09-19 16:31:30 -0400

    Create slideout JS

---
+++ ultimate-pc/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    //toggle the component with class slideout
+    $(".box").click(function() {
+      if ($('.slideout').is(':visible')) {
+        $(".slideout").slideUp(300);
+        $(".plusminus").text('+');
+      }
+      if ($(this).next(".slideout").is(':visible')) {
+        $(this).next(".slideout").slideUp(300);
+        $(this).children(".plusminus").text('+');
+      } else {
+        $(this).next(".slideout").slideDown(300);
+        $(this).children(".plusminus").text('-');
+      }
+    });
+  });
